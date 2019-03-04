ac0de9b80082457dd6efe3391ab2fa051cb86a08
Alex Moreno <alex.moreno@linecorp.com>
2019-03-04 16:07:03 +0800

    add Travis-CI

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,19 +2,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Lib (app)
+import Line.Bot.Client
 import Test.Hspec
 import Test.Hspec.Wai
 import Test.Hspec.Wai.JSON
 
 main :: IO ()
-main = hspec spec
-
-spec :: Spec
-spec = with (return app) $ do
-    describe "GET /users" $ do
-        it "responds with 200" $ do
-            get "/users" `shouldRespondWith` 200
-        it "responds with [User]" $ do
-            let users = "[{\"userId\":1,\"userFirstName\":\"Isaac\",\"userLastName\":\"Newton\"},{\"userId\":2,\"userFirstName\":\"Albert\",\"userLastName\":\"Einstein\"}]"
-            get "/users" `shouldRespondWith` users
+main = return ()
